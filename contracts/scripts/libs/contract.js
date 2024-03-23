cf5de0bfffbbb4565b9f7c3797c22633fa1f9d83
Vojtch159 <vojta159.studeny@gmail.com>
2024-03-23 09:24:16 +0100

    add devnet deploy script

--- contracts/scripts/libs/contract.js
+++ contracts/scripts/libs/contract.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import * as fs from "fs";
-import { json } from "starknet";
-import { getNetwork, getAccount } from "./network.js";
+import { json, CallData, cairo, byteArray } from "starknet";
+import { getAccount } from "./network.js";
 
 const declare = async (filepath, contract_name) => {
   console.log(`\nDeclaring ${contract_name}...`);
@@ -19,7 +19,6 @@ const declare = async (filepath, contract_name) => {
     casm: compiledSierraCasmFile,
   });
 
-  const network = getNetwork(process.env.STARKNET_NETWORK);
   console.log(`- Class Hash: ${contract.class_hash}`);
   if (contract.transaction_hash) {
     console.log(
@@ -33,7 +32,7 @@ const declare = async (filepath, contract_name) => {
   return contract;
 };
 
-export const deployBundler = async (path) => {
+export const deployBundler = async () => {
   const account = getAccount();
   const bundler = await declare(process.env.PATH_TO_CASM_COMPILE, "TokenBundler");
 
@@ -48,8 +47,71 @@ export const deployBundler = async (path) => {
     ],
   });
 
-  const network = getNetwork(process.env.STARKNET_NETWORK);
   console.log(`Tx hash: ${contract.transaction_hash}`);
   await account.waitForTransaction(contract.transaction_hash);
   console.log(`Bundler deployed at ${contract.address}`);
+};
+
+export const deployMockERC20 = async () => {
+  const account = getAccount();
+  const erc20 = await declare(process.env.ERC20_PATH, "MockERC20");
+
+  console.log(`\nDeploying Mock ERC20...`);
+  console.log("Owner: ", process.env.STARKNET_ACCOUNT_ADDRESS);
+  console.log("ERC20 class hash: ", erc20.class_hash);
+
+  const contract = await account.deployContract({
+    classHash: erc20.class_hash,
+    constructorCalldata: CallData.compile({
+      initial_supply: cairo.uint256(21000000n * 10n ** 18n),
+      recipient: process.env.STARKNET_ACCOUNT_ADDRESS,
+    }),
+  });
+
+  console.log(`Tx hash: ${contract.transaction_hash}`);
+  await account.waitForTransaction(contract.transaction_hash);
+  console.log(`ERC20 deployed at ${contract.address}`);
+};
+
+export const deployMockERC721 = async () => {
+  const account = getAccount();
+  const erc721 = await declare(process.env.ERC721_PATH, "MockERC721");
+
+  console.log(`\nDeploying Mock ERC721...`);
+  console.log("Owner: ", process.env.STARKNET_ACCOUNT_ADDRESS);
+  console.log("ERC721 class hash: ", erc721.class_hash);
+
+  const contract = await account.deployContract({
+    classHash: erc721.class_hash,
+    constructorCalldata: [
+      process.env.STARKNET_ACCOUNT_ADDRESS,
+    ],
+  });
+
+  console.log(`Tx hash: ${contract.transaction_hash}`);
+  await account.waitForTransaction(contract.transaction_hash);
+  console.log(`ERC721 deployed at ${contract.address}`);
+};
+
+export const deployMockERC1155 = async () => {
+  const account = getAccount();
+  const erc1155 = await declare(process.env.ERC1155_PATH, "MockERC1155");
+
+  console.log(`\nDeploying Mock ERC1155...`);
+  console.log("Owner: ", process.env.STARKNET_ACCOUNT_ADDRESS);
+  console.log("ERC1155 class hash: ", erc1155.class_hash);
+
+  const contract = await account.deployContract({
+    classHash: erc1155.class_hash,
+    constructorCalldata: CallData.compile({
+      token_uri: byteArray.byteArrayFromString('token_uri'),
+      recipient: process.env.STARKNET_ACCOUNT_ADDRESS,
+      token_ids: [cairo.uint256(1)],
+      values: [cairo.uint256(1)]
+    })
+  });
+
+  console.log(`Tx hash: ${contract.transaction_hash}`);
+  await account.waitForTransaction(contract.transaction_hash);
+  console.log(`ERC1155 deployed at ${contract.address}`);
 };
